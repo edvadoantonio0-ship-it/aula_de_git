67058136e2e5a24e673323f5bcd47fb33aa5cd12
Edvaldo <edvadoantonio0@gmail.com>
2026-06-07 00:00:26 +0100

    Adicionei uma branch cal_iadade

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
 function somar(){
     console.log(1 + 1);
+} 
+
+function calcularIdade(anoNascimento) {
+    const anoAtual = new Date().getFullYear();
+    return anoAtual - anoNascimento;
 }
+
+console.log(calcularIdade(2005));
